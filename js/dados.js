9e4758cdd9bceca4d81b3a1c92160a79f9e70f32
Joao <jl2080827@gmail.com>
2024-08-25 18:23:09 -0300

    s
    s

---
+++ js/dados.js
@@ -0,0 +1,46 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "images/carne1.jpg",
+            "name": "Joe's KC BBQ",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 110.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "images/carne2.jpg",
+            "name": "Kings BBQ",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 89
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "images/carne3.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 199
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "images/carne4.jpg",
+            "name": "Franklin Barbecue",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 249
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "images/carne5.jpg",
+            "name": "Blue Smoke",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "images/carne6.jpg",
+            "name": "Blue Smoke",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+    ]
+}
